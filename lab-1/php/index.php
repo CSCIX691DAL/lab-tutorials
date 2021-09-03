3b783ec21931da7007cc3efd71eb20b9aa8af62f
Library User <lib-user@cheb-maclap3.local>
2021-09-03 14:11:59 -0300

    init

---
+++ lab-1/php/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CSCI 2690 - PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+        $msg = "Here we go";
+        echo $msg;
+        ?>
+    </h1>
+</body>
+</html>
